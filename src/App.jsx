aa3ab04b97f085af8c068df4e456069dfdb95d5e
Boris Chabirand <contact@boris-chabirand.fr>
2023-10-12 23:38:14 +0200

    Add codeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { useEffect, useLayoutEffect, useRef } from "react"
 import { Canvas, useFrame, useThree } from "@react-three/fiber"
-import { useGLTF, useAnimations, useScroll, ScrollControls } from "@react-three/drei"
+import { useGLTF, useAnimations, useScroll, ScrollControls, Box } from "@react-three/drei"
 import { gsap } from 'gsap'
 
 function Model(props) {
@@ -25,7 +25,6 @@ function Model(props) {
 
     // Character movement
     tl.current.to(ref.current.position, { duration: 1, x: -3 }, 0.05)
-    .call(console.log("COUCOU"))
     // tl.current.to(ref.current.position, { duration: 1, y: -3 }, ">");
   }, []);
 
@@ -39,13 +38,23 @@ function Model(props) {
   )
 }
 
+function CodeLine(props) {
+  const colors = ['red', 'green', 'blue', 'orange', 'yellow']
+  const random = Math.floor(Math.random() * 5)
+
+  return (
+    <Box args={[10, 0.3, 3]} position={[0, -1.3, 0]} material={new THREE.MeshPhongMaterial()} material-color={colors[random]} />
+  )
+}
+
 const App = () => (
-  <Canvas shadows gl={{ antialias: true }} camera={{ position: [0, 0, -15], fov: 60 }}>
+  <Canvas shadows gl={{ antialias: true }} camera={{ position: [0, 0, -10], fov: 60 }}>
     <color attach="background" args={["#1F1F1F"]} />
     <directionalLight intensity={2} position={[0, 20, -30]} castShadow shadow-mapSize={1024} shadow-bias={-0.0001} />
     <ambientLight intensity={1} />
       <ScrollControls pages={1} damping={0.25}>
         <Model position={[0, -1, 0]} rotation={[0, Math.PI / 2, 0]}/>
+        <CodeLine />
       </ScrollControls>
   </Canvas>
 )
